14135b053bcdea3fc007d84996f7ceb63e3208b7
Bruno Campagnol <bruno_unky@hotmail.com>
2024-09-18 16:33:32 -0300

    add login user

---
+++ KeyControl/projeto/public/verifica_login.php
@@ -0,0 +1,33 @@
+<?php
+require 'db_conexao.php';
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuarios WHERE email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    
+    try {
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($senha, $user['senha'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_name'] = $user['nome'];
+            header("Location: testelogin.php");
+            exit();
+        } else {
+            echo "E-mail ou senha incorretos.";
+        }
+    } catch (PDOException $e) {
+        echo "Erro ao acessar o banco de dados: " . $e->getMessage();
+    }
+} else {
+    header("Location: login.html");
+    exit();
+}
+?>
