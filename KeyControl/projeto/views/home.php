d8c6ddf730c71c58da3c682967399ba126d5bc55
Beatriz <beatrizm.oli0102@gmail.com>
2024-10-21 17:01:05 -0300

    ajustes na home e css

--- KeyControl/projeto/views/home.php
+++ KeyControl/projeto/views/home.php
@@ -33,13 +33,11 @@
 
 
 <div class="container">
-    <h2>Bem-vindo, <?php echo htmlspecialchars($_SESSION['user_name']); ?>!</h2>
-    <p>E-mail: <?php echo htmlspecialchars($_SESSION['user_email']); ?></p>
-    <div class="buttons">
-
-    </div>
-    <p><a href="../app/controllers/logout.php">Sair</a></p>
-</div>
+    <h2 style="color: white;">Bem-vindo, <?php echo htmlspecialchars($_SESSION['user_name']); ?>!</h2>
+    <section id="home">
+        <img src="../img/">
+    </section> 
+</div> 
    
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
