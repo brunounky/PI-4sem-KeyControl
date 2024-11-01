008ccb8281f0d921c1733731b7603f6b728565b6
Beatriz <beatrizm.oli0102@gmail.com>
2024-11-01 16:57:40 -0300

    Att fechamento

--- KeyControl/projeto/views/fechamento.php
+++ KeyControl/projeto/views/fechamento.php
@@ -122,8 +122,30 @@
                     <td>Araras</td>
                     <td>Locatário</td>
                     <td>3</td>
-                    <td><button class='' ><i class="bi bi-ticket-detailed-fill"></i></button></td>
+                    <td> 
+                        <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <i class="bi bi-ticket-detailed-fill"></i>
+                        </button> 
 
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                              </div>
+                              <div class="modal-body">
+                                ...
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </td>
                 </tr>
                 
                 <!--
