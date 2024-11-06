fe8ba2d67a35098dba20c9c7f3df34bf7d978975
Bruno Campagnol <bruno_unky@hotmail.com>
2024-11-06 17:31:46 -0300

    Alteracao de heranca banco de dados

--- KeyControl/projeto/app/controllers/verifica_cliente.php
+++ KeyControl/projeto/app/controllers/verifica_cliente.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: ../app/controllers/verifica_login.php");
+    exit();
+}
+
 require '../controllers/db_conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'cadastrar') {
@@ -24,10 +31,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
     $fiador = isset($_POST['fiador']) ? 1 : 0;
     $comprador = isset($_POST['comprador']) ? 1 : 0;
 
-  
+    $id_imobiliaria = $_SESSION['user_cnpj'];
 
-    $stmt = $pdo->prepare("INSERT INTO cadastro_cliente (nome, cpf_cnpj, telefone, data_nascimento_fundacao, rg_ie, email, nacionalidade, estado_civil, profissao, cep, rua, numero, bairro, complemento, cidade, estado, pais, locador, locatario, fiador, comprador) 
-                            VALUES (:nome, :cpf_cnpj, :telefone, :data_nascimento_fundacao, :rg_ie, :email, :nacionalidade, :estado_civil, :profissao, :cep, :rua, :numero, :bairro, :complemento, :cidade, :estado, :pais, :locador, :locatario, :fiador, :comprador)");
+    $stmt = $pdo->prepare("INSERT INTO cadastro_cliente (nome, cpf_cnpj, telefone, data_nascimento_fundacao, rg_ie, email, nacionalidade, estado_civil, profissao, cep, rua, numero, bairro, complemento, cidade, estado, pais, locador, locatario, fiador, comprador, id_imobiliaria) 
+                            VALUES (:nome, :cpf_cnpj, :telefone, :data_nascimento_fundacao, :rg_ie, :email, :nacionalidade, :estado_civil, :profissao, :cep, :rua, :numero, :bairro, :complemento, :cidade, :estado, :pais, :locador, :locatario, :fiador, :comprador, :id_imobiliaria)");
 
     $stmt->bindParam(':nome', $nome);
     $stmt->bindParam(':cpf_cnpj', $cpf_cnpj);
@@ -50,6 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
     $stmt->bindParam(':locatario', $locatario);
     $stmt->bindParam(':fiador', $fiador);
     $stmt->bindParam(':comprador', $comprador);
+    $stmt->bindParam(':id_imobiliaria', $id_imobiliaria);
 
     if ($stmt->execute()) {
         header("Location: ../../views/lista_cliente.php");
