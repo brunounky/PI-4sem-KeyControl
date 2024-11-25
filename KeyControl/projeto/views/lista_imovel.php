5a7e0804631b48ca2f18c214ef9969c1fd1ae920
Maria Vitoria <maria.suzarth@hotmail.com>
2024-11-25 13:04:56 -0300

    incluindo script remover_filtro

--- KeyControl/projeto/views/lista_imovel.php
+++ KeyControl/projeto/views/lista_imovel.php
@@ -141,6 +141,8 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
       <script src="../public/assets/js/consultacep.js"></script>
       <script src="../public/assets/js/submenu.js"></script>
+      <script src="../public/assets/js/remover_filtro.js"></script>
+
 
       <script>
          function redirectToRelatorio(id) {
