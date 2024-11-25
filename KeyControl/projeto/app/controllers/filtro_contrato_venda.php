d0aa7befe97d680a48ecb96d7338e4a0f041157a
Maria Vitoria <maria.suzarth@hotmail.com>
2024-11-25 13:04:01 -0300

    Listagem contrato venda corrigido

--- KeyControl/projeto/app/controllers/filtro_contrato_venda.php
+++ KeyControl/projeto/app/controllers/filtro_contrato_venda.php
@@ -8,6 +8,10 @@ function buildQuery($contrato_id, $nome, $tipo_imovel, $forma_pagamento) {
         SELECT DISTINCT 
             cv.id AS contrato_id,
             cv.comprador_nome,
+            cv.imovel_taxa_venda,
+            cv.data_emissao,
+            cv.data_vencimento,
+            cv.imovel_valor,
             cc.nome AS comprador_nome_completo,
             cc.cpf_cnpj AS comprador_cpf_cnpj,
             ci.cpf_cnpj_proprietario AS cpf_cnpj_proprietario,
@@ -27,6 +31,18 @@ function buildQuery($contrato_id, $nome, $tipo_imovel, $forma_pagamento) {
     if (!empty($contrato_id)) {
         $sql .= " AND cv.id = :contrato_id"; 
     }
+    if (!empty($imovel_taxa_venda)) {
+        $sql .= " AND cv.imovel_taxa_venda = :imovel_taxa_venda"; 
+    }
+    if (!empty($data_emissao)) {
+        $sql .= " AND cv.data_emissao = :data_emissao"; 
+    }
+    if (!empty($data_vencimento)) {
+        $sql .= " AND cv.data_vencimento = :data_vencimento"; 
+    }
+    if (!empty($imovel_valor)) {
+        $sql .= " AND cv.imovel_valor = :imovel_valor"; 
+    }
     if (!empty($tipo_imovel)) {
         $sql .= " AND ci.tipo_imovel = :tipo_imovel"; 
     }
@@ -39,15 +55,23 @@ function buildQuery($contrato_id, $nome, $tipo_imovel, $forma_pagamento) {
 
 $contrato_id = $_POST['contrato_id'] ?? '';
 $nome = $_POST['nome'] ?? '';
+$imovel_taxa_venda = $_POST['imovel_taxa_venda'] ?? '';
+$data_emissao = $_POST['data_emissao'] ?? '';
+$data_vencimento = $_POST['data_vencimento'] ?? '';
+$imovel_valor = $_POST['imovel_valor'] ?? '';
 $tipo_imovel = $_POST['tipo_imovel'] ?? '';
 $forma_pagamento = $_POST['forma_pagamento'] ?? '';
 
 $contrato_id = htmlspecialchars($contrato_id);
 $nome = htmlspecialchars($nome);
+$imovel_taxa_venda = htmlspecialchars($imovel_taxa_venda);
+$data_emissao = htmlspecialchars($data_emissao);
+$data_vencimento = htmlspecialchars($data_vencimento);
+$imovel_valor = htmlspecialchars($imovel_valor);
 $tipo_imovel = htmlspecialchars($tipo_imovel);
 $forma_pagamento = htmlspecialchars($forma_pagamento);
 
-$sql = buildQuery($contrato_id, $nome, $tipo_imovel, $forma_pagamento);
+$sql = buildQuery($contrato_id, $nome, $imovel_taxa_venda, $data_emissao, $data_vencimento, $imovel_valor,  $tipo_imovel, $forma_pagamento);
 
 try {
     $stmt = $pdo->prepare($sql);
@@ -57,6 +81,22 @@ try {
     if (!empty($nome)) {
         $nome = "%$nome%";
         $stmt->bindParam(':nome', $nome);
+    }
+    if (!empty($imovel_taxa_venda)) {
+        $imovel_taxa_venda = "%$imovel_taxa_venda%";
+        $stmt->bindParam(':imovel_taxa_venda', $imovel_taxa_venda);
+    }
+    if (!empty($data_emissao)) {
+        $data_emissao = "%$data_emissao%";
+        $stmt->bindParam(':data_emissao', $data_emissao);
+    }
+    if (!empty($data_vencimento)) {
+        $data_vencimento = "%$data_vencimento%";
+        $stmt->bindParam(':data_vencimento', $data_vencimento);
+    }    
+    if (!empty($imovel_valor)) {
+        $imovel_valor = "%$imovel_valor%";
+        $stmt->bindParam(':imovel_valor', $imovel_valor);
     }
     if (!empty($tipo_imovel)) {
         $stmt->bindParam(':tipo_imovel', $tipo_imovel);
--- KeyControl/projeto/views/lista_contrato_venda.php
+++ KeyControl/projeto/views/lista_contrato_venda.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 if (!isset($_SESSION['user_id'])) {
@@ -10,6 +10,7 @@ include_once '../app/controllers/filtro_contrato_venda.php';
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -20,6 +21,7 @@ include_once '../app/controllers/filtro_contrato_venda.php';
     <link rel="icon" href="../public/assets/img/Logotipo.svg">
     <title>Contratos de Venda</title>
 </head>
+
 <body>
     <?php include 'navbar.php'; ?>
     <section>
@@ -35,43 +37,60 @@ include_once '../app/controllers/filtro_contrato_venda.php';
                     <div class="row g-12">
                         <div class="col-md-1">
                             <label for="contrato_id" class="form-label">ID</label>
-                            <input type="text" id="contrato_id" class="form-control" name="contrato_id" value="<?= htmlspecialchars($_POST['contrato_id'] ?? '') ?>">
+                            <input type="text" id="contrato_id" class="form-control" name="contrato_id"
+                                value="<?= htmlspecialchars($_POST['contrato_id'] ?? '') ?>">
                         </div>
                         <div class="col-md-2">
                             <label for="nome" class="form-label">Comprador</label>
-                            <input type="text" id="nome" class="form-control" name="nome" value="<?= htmlspecialchars($_POST['nome'] ?? '') ?>">
+                            <input type="text" id="nome" class="form-control" name="nome"
+                                value="<?= htmlspecialchars($_POST['nome'] ?? '') ?>">
                         </div>
                         <div class="col-md-2">
                             <label for="vigencia" class="form-label">Vigência</label>
-                            <input type="text" id="vigencia" class="form-control" name="vigencia" value="<?= htmlspecialchars($_POST['vigencia'] ?? '') ?>">
+                            <input type="text" id="vigencia" class="form-control" name="vigencia"
+                                value="<?= htmlspecialchars($_POST['vigencia'] ?? '') ?>">
                         </div>
                         <div class="col-md-2">
                             <label for="dia_pagamento" class="form-label">Pagamento</label>
-                            <input type="text" id="dia_pagamento" class="form-control" name="dia_pagamento" value="<?= htmlspecialchars($_POST['dia_pagamento'] ?? '') ?>">
+                            <input type="text" id="dia_pagamento" class="form-control" name="dia_pagamento"
+                                value="<?= htmlspecialchars($_POST['dia_pagamento'] ?? '') ?>">
                         </div>
                         <div class="col-md-2">
-                            <label for="tipo_imovel" class="mb-2">Tipo Imóvel</label>
-                            <select class="form-control" name="tipo_imovel" id="tipo_imovel" onchange="checkSelection('tipo_imovel')">
-                                <option value="" disabled <?= !isset($_POST['tipo_imovel']) ? 'selected' : '' ?>>Selecione um tipo</option>
-                                <option value="apartamento" <?= ($_POST['tipo_imovel'] ?? '') == 'apartamento' ? 'selected' : '' ?>>Apartamento</option>
-                                <option value="casa" <?= ($_POST['tipo_imovel'] ?? '') == 'casa' ? 'selected' : '' ?>>Casa</option>
-                                <option value="comercial" <?= ($_POST['tipo_imovel'] ?? '') == 'comercial' ? 'selected' : '' ?>>Comercial</option>
-                            </select>
-                            <span class="position-absolute" style="right: 20px; top: 8px; cursor: pointer; color: red; display: <?= isset($_POST['tipo_imovel']) && $_POST['tipo_imovel'] != '' ? 'block' : 'none' ?>;" data-select="tipo_imovel" onclick="removeSelected('tipo_imovel')">x</span>
+                            <label for="tipo_imovel" class="mb-2">Tipo do Imóvel</label>
+                            <div class="position-relative">
+                                <select class="form-control" name="tipo_imovel" id="tipo_imovel"
+                                    onchange="checkSelection('tipo_imovel')">
+                                    <option value="" disabled <?= !isset($_POST['tipo_imovel']) ? 'selected' : '' ?>>
+                                        Escolha um Tipo</option>
+                                    <option value="apartamento" <?= ($_POST['tipo_imovel'] ?? '') == 'apartamento' ? 'selected' : '' ?>>Apartamento</option>
+                                    <option value="casa" <?= ($_POST['tipo_imovel'] ?? '') == 'casa' ? 'selected' : '' ?>>
+                                        Casa</option>
+                                    <option value="comercial" <?= ($_POST['tipo_imovel'] ?? '') == 'comercial' ? 'selected' : '' ?>>Comercial</option>
+                                </select>
+                                <span class="position-absolute"
+                                    style="right: 20px; top: 6px; cursor: pointer; color: red; display: <?= isset($_POST['tipo_imovel']) && $_POST['tipo_imovel'] != '' ? 'block' : 'none' ?>;"
+                                    data-select="tipo_imovel" onclick="removeSelected('tipo_imovel')">x</span>
+                            </div>
                         </div>
+
                         <div class="col-sm-2">
                             <label for="forma_pagamento" class="mb-2">Tipo Pagamento</label>
-                            <select class="form-control mb-3" name="contrato_forma_pagamento" id="forma_pagamento">
-                                <option value="" disabled <?= !isset($_POST['forma_pagamento']) ? 'selected' : '' ?>>Escolha um Pagamento</option>
-                                <option value="financiamento" <?= ($_POST['forma_pagamento'] ?? '') == 'financiamento' ? 'selected' : '' ?>>Financiamento</option>
-                                <option value="dinheiro" <?= ($_POST['forma_pagamento'] ?? '') == 'dinheiro' ? 'selected' : '' ?>>Dinheiro</option>
-                                <option value="boleto" <?= ($_POST['forma_pagamento'] ?? '') == 'boleto' ? 'selected' : '' ?>>Boleto</option>
-                                <option value="pix" <?= ($_POST['forma_pagamento'] ?? '') == 'pix' ? 'selected' : '' ?>>PIX</option>
-                                <option value="transferencia" <?= ($_POST['forma_pagamento'] ?? '') == 'transferencia' ? 'selected' : '' ?>>Transferência</option>
-                                <option value="cartao_credito" <?= ($_POST['forma_pagamento'] ?? '') == 'cartao_credito' ? 'selected' : '' ?>>Cartão de crédito</option>
-                                <option value="cartao_debito" <?= ($_POST['forma_pagamento'] ?? '') == 'cartao_debito' ? 'selected' : '' ?>>Cartão de débito</option>
-                            </select>
-                            <span class="position-absolute" style="right: 25px; top: 40px; cursor: pointer; color: red; display: <?= isset($_POST['forma_pagamento']) && $_POST['contrato_forma_pagamento'] != '' ? 'block' : 'none' ?>;" data-select="forma_pagamento" onclick="removeSelected('forma_pagamento')">x</span>
+                            <div class="position-relative">
+                                <select class="form-control mb-3" name="contrato_forma_pagamento" id="forma_pagamento"
+                                    onchange="checkSelection('forma_pagamento')">
+                                    <option value="" disabled <?= !isset($_POST['contrato_forma_pagamento']) ? 'selected' : '' ?>>Escolha um Pagamento</option>
+                                    <option value="financiamento" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'financiamento' ? 'selected' : '' ?>>Financiamento</option>
+                                    <option value="dinheiro" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'dinheiro' ? 'selected' : '' ?>>Dinheiro</option>
+                                    <option value="boleto" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'boleto' ? 'selected' : '' ?>>Boleto</option>
+                                    <option value="pix" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'pix' ? 'selected' : '' ?>>PIX</option>
+                                    <option value="transferencia" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'transferencia' ? 'selected' : '' ?>>Transferência</option>
+                                    <option value="cartao_credito" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'cartao_credito' ? 'selected' : '' ?>>Cartão de crédito</option>
+                                    <option value="cartao_debito" <?= ($_POST['contrato_forma_pagamento'] ?? '') == 'cartao_debito' ? 'selected' : '' ?>>Cartão de débito</option>
+                                </select>
+                                <span class="position-absolute"
+                                    style="right: 25px; top: 6px; cursor: pointer; color: red; display: <?= isset($_POST['contrato_forma_pagamento']) && $_POST['contrato_forma_pagamento'] != '' ? 'block' : 'none' ?>;"
+                                    data-select="forma_pagamento" onclick="removeSelected('forma_pagamento')">x</span>
+                            </div>
                         </div>
                         <div class="col-md-1">
                             <button class="btn btn-buscar" type="submit">
@@ -91,8 +110,11 @@ include_once '../app/controllers/filtro_contrato_venda.php';
                         <tr>
                             <th>ID Contrato</th>
                             <th>Comprador</th>
-                            <th>Data Vigência</th>
-                            <th>Contrato Vencimento</th>
+                            <th>% Taxa Adm</th>
+                            <th>Data Emissão</th>
+                            <th>Data Vencimento</</th>
+                            <th>Valor Imóvel</th>
+                            <th>Forma Pagamento</th>
                             <th></th>
                         </tr>
                     </thead>
@@ -101,31 +123,35 @@ include_once '../app/controllers/filtro_contrato_venda.php';
                     if (isset($result) && count($result) > 0) {
                         foreach ($result as $row) {
                             echo "<tr>
-                                    <td>" . htmlspecialchars($row['contrato_id'] ?? '-') . "</td>
-                                    <td>" . htmlspecialchars($row['comprador_nome'] ?? '-') . "</td>
-                                    <td>" . htmlspecialchars($row['forma_pagamento'] ?? '-') . "</td>
-                                    <td>
-                                        <button class='btn' onclick=\"window.location.href='../views/modifica_contrato_venda.php?id=" . htmlspecialchars($row['contrato_id']) . "'\">
-                                          <i class='bi bi-pencil-square'></i>
-                                          </button>
-                                        <button class='btn' onclick='toggleSubMenu(this)'>
-                                            <i class='bi bi-chevron-down'></i>
-                                        </button>
-                                            <div class='submenu' style='display: none;'>
-                                            <div class='submenu-options'>
-                                                <button class='imprimir' title='Imprimir' onclick='redirectToRelatorio(" . htmlspecialchars($row['contrato_id']) . ")'>
-                                                    <i class='bi bi-printer'></i> Imprimir
-                                                </button>
-                                            </div>
+                                <td>" . htmlspecialchars($row['contrato_id'] ?? '-') . "</td>
+                                <td>" . htmlspecialchars($row['comprador_nome'] ?? '-') . "</td>
+                                <td>" . htmlspecialchars($row['imovel_taxa_venda'] ?? '-') . "</td>
+                                <td>" . htmlspecialchars(date("d/m/Y", strtotime($row['data_emissao'] ?? ''))) . "</td>
+                                <td>" . htmlspecialchars(date("d/m/Y", strtotime($row['data_vencimento'] ?? ''))) . "</td>
+                                <td>" . htmlspecialchars($row['imovel_valor'] ?? '-') . "</td>
+                                <td>" . htmlspecialchars($row['forma_pagamento'] ?? '-') . "</td>
+                                <td>
+                                    <button class='btn' onclick=\"window.location.href='../views/modifica_contrato_venda.php?id=" . htmlspecialchars($row['contrato_id']) . "'\">
+                                        <i class='bi bi-pencil-square'></i>
+                                    </button>
+                                    <button class='btn' onclick='toggleSubMenu(this)'>
+                                        <i class='bi bi-chevron-down'></i>
+                                    </button>
+                                    <div class='submenu' style='display: none;'>
+                                        <div class='submenu-options'>
+                                            <button class='imprimir' title='Imprimir' onclick='redirectToRelatorio(" . htmlspecialchars($row['contrato_id']) . ")'>
+                                                <i class='bi bi-printer'></i> Imprimir
+                                            </button>
                                         </div>
-                                    </td>
-                                </tr>";
+                                    </div>
+                                </td>
+                            </tr>";
                         }
                     } else {
                         echo "<tr><td colspan='6'>Nenhum registro encontrado</td></tr>";
                     }
                     ?>
-                    </tbody>
+                </tbody>
                 </table>
             </div>
         </div>
@@ -133,16 +159,15 @@ include_once '../app/controllers/filtro_contrato_venda.php';
     <script src="../public/assets/js/menu.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../public/assets/js/submenu.js"></script>
+    <script src="../public/assets/js/remover_filtro.js"></script>
+
+
     <script>
-    function redirectToRelatorio(id) {
-        window.location.href = '../reports/impressao_contrato_venda.php?id=' + id;
-    }
-//     function deleteRecord(id) {
-//       if (confirm('Você realmente deseja excluir este cliente?')) {
-//          window.location.href = '../app/controllers/apaga_imovel.php?id=' + id;
-//       }
-//    }
-</script>
+        function redirectToRelatorio(id) {
+            window.open('../reports/impressao_contrato_venda.php?id=' + id, '_blank');
+        }
+    </script>
 
 </body>
+
 </html>
