7a93cf561b79c5ec5e69daeebd61da0037d06a98
Bruno Campagnol <bruno_unky@hotmail.com>
2024-11-26 19:52:34 -0300

    Update keycontroldb.sql

--- Documentação/Banco de dados/keycontroldb.sql
+++ Documentação/Banco de dados/keycontroldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26/11/2024 às 04:51
+-- Tempo de geração: 26/11/2024 às 23:50
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -172,7 +172,10 @@ CREATE TABLE `contrato_aluguel` (
 INSERT INTO `contrato_aluguel` (`id`, `locatario_nome`, `locatario_data_nascimento`, `locatario_nacionalidade`, `locatario_cep`, `locatario_bairro`, `locatario_estado`, `locatario_cpf_cnpj`, `locatario_telefone`, `locatario_estado_civil`, `locatario_rua`, `locatario_complemento`, `locatario_pais`, `locatario_rg_ie`, `locatario_email`, `locatario_profissao`, `locatario_numero`, `locatario_cidade`, `imovel_proprietario_cpf_cnpj`, `imovel_tipo`, `imovel_numero`, `imovel_cidade`, `imovel_taxa_venda`, `imovel_cep`, `imovel_bairro`, `imovel_estado`, `imovel_valor`, `imovel_registro`, `imovel_rua`, `imovel_complemento`, `imovel_pais`, `fiador_nome`, `fiador_data_nascimento`, `fiador_nacionalidade`, `fiador_cep`, `fiador_bairro`, `fiador_estado`, `fiador_cpf_cnpj`, `fiador_telefone`, `fiador_estado_civil`, `fiador_rua`, `fiador_complemento`, `fiador_pais`, `fiador_rg_ie`, `fiador_email`, `fiador_profissao`, `fiador_numero`, `fiador_cidade`, `contrato_vigencia`, `contrato_forma_pagamento`, `contrato_data_vencimento`, `id_imobiliaria`, `meses_caucao`, `vencimento_caucao`, `total_caucao`, `forma_pagamento_caucao`, `id_lancamento`) VALUES
 (8, 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 111.00, '13506180', 'Parque Mãe Preta', 'SP', 12.00, '22', 'Rua 10 MP', '1', 'Brasil', 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '2024-12-06', 'Boleto', '2024-11-08', '73943371000180', NULL, NULL, NULL, NULL, 0),
 (9, 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 111.00, '13506180', 'Parque Mãe Preta', 'SP', 500001.00, '22', 'Rua 10 MP', '1', 'Brasil', 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '2024-12-04', 'Cartão de débito', '2024-11-22', '73943371000180', NULL, NULL, NULL, NULL, 0),
-(10, 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 111.00, '13506180', 'Parque Mãe Preta', 'SP', 12.00, '22', 'Rua 10 MP', '1', 'Brasil', 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '2024-11-27', 'PIX', '2024-12-06', '73943371000180', NULL, NULL, NULL, NULL, 18);
+(10, 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 111.00, '13506180', 'Parque Mãe Preta', 'SP', 12.00, '22', 'Rua 10 MP', '1', 'Brasil', 'Guilherme Ribeiro Bonatti', '2005-03-26', 'Brasileiro', '13505493', 'Jardim Residencial São José', 'SP', '39911823865', '19996236443', 'Solteiro', 'Avenida M 49', 'Portao cinza', 'Brasil', '640022947', 'guilhermeribeirobonatti@gmail.com', 'Operador de caixa', 2430, 'Rio Claro', '2024-11-27', 'PIX', '2024-12-06', '73943371000180', NULL, NULL, NULL, NULL, 18),
+(16, 'vitoria lucianetti dos santos', '2024-12-07', '4', '13506-560', 'Vila Nova', 'SP', '54234346578', '19996886205', 'SP', 'Rua 16 A', '4', 'Brasil', '44234432', '627sp@al.educacao.sp.gov.bd', '4', 16, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 11.00, '13506180', 'Parque Mãe Preta', 'SP', 11.00, '22', 'Rua 10 MP', '1', 'Brasil', '', '0000-00-00', '', '', '', '', '', '', '', '', NULL, '', '', '', '', 0, '', '2024-11-29', 'Cartão de crédito', '2024-11-28', '73943371000180', 11, '2024-12-06', 2222222.00, 'Boleto', 35),
+(17, 'vitoria lucianetti dos santos', '2024-12-07', '4', '13506-560', 'Vila Nova', 'SP', '54234346578', '19996886205', 'SP', 'Rua 16 A', '4', 'Brasil', '44234432', '627sp@al.educacao.sp.gov.bd', '4', 16, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 11.00, '13506180', 'Parque Mãe Preta', 'SP', 11.00, '22', 'Rua 10 MP', '1', 'Brasil', '', '0000-00-00', '', '', '', '', '', '', '', '', NULL, '', '', '', '', 0, '', '2024-11-29', 'Cartão de crédito', '2024-12-06', '73943371000180', 1, '2024-11-07', 11.00, 'PIX', 37),
+(18, 'vitoria lucianetti dos santos', '2024-12-07', '4', '13506-560', 'Vila Nova', 'SP', '54234346578', '19996886205', 'SP', 'Rua 16 A', '4', 'Brasil', '44234432', '627sp@al.educacao.sp.gov.bd', '4', 16, 'Rio Claro', '54234346578', 'apartamento', 1, 'Rio Claro', 11.00, '13506180', 'Parque Mãe Preta', 'SP', 11.00, '22', 'Rua 10 MP', '1', 'Brasil', '', '0000-00-00', '', '', '', '', '', '', '', '', NULL, '', '', '', '', 0, '', '2024-12-06', 'Cartão de crédito', '2024-11-29', '73943371000180', 10, '2024-12-06', 110.00, 'PIX', 40);
 
 -- --------------------------------------------------------
 
@@ -268,23 +271,35 @@ CREATE TABLE `lancamento_financeiro` (
   `forma_pagamento` varchar(20) DEFAULT NULL,
   `observacoes` varchar(255) DEFAULT NULL,
   `data_vigencia` date DEFAULT NULL,
-  `meses_caucao` int(11) DEFAULT NULL
+  `meses_caucao` int(11) DEFAULT NULL,
+  `liquidado` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `lancamento_financeiro`
 --
 
-INSERT INTO `lancamento_financeiro` (`id_lancamento`, `id_imobiliaria`, `tipo_lancamento`, `registro_imovel`, `data_emissao`, `data_vencimento`, `valor_total`, `forma_pagamento`, `observacoes`, `data_vigencia`, `meses_caucao`) VALUES
-(8, '73943371000180', 'venda imovel', '22', '2024-11-25', '2024-12-04', 12, 'Cartão de crédito', NULL, NULL, NULL),
-(10, '73943371000180', 'venda imovel', '22', '2024-11-20', '2024-12-05', 12, 'Cartão de débito', NULL, NULL, NULL),
-(12, '73943371000180', 'venda imovel', '22', '2024-11-25', '2024-11-27', 12, 'Cartão de crédito', NULL, NULL, NULL),
-(13, '73943371000180', 'Aluguel', '22', '2024-11-06', '2024-10-30', 33, 'Dinheiro', '3', NULL, NULL),
-(14, '73943371000180', 'venda imovel', '22', '2024-11-25', '2024-11-29', 12, 'PIX', NULL, NULL, NULL),
-(15, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-22', NULL, 'Boleto', NULL, '2024-11-06', NULL),
-(16, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-08', 12, 'Boleto', NULL, '2024-12-06', NULL),
-(17, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-22', 500001, 'Cartão de débito', NULL, '2024-12-04', NULL),
-(18, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-12-06', 12, 'PIX', NULL, '2024-11-27', NULL);
+INSERT INTO `lancamento_financeiro` (`id_lancamento`, `id_imobiliaria`, `tipo_lancamento`, `registro_imovel`, `data_emissao`, `data_vencimento`, `valor_total`, `forma_pagamento`, `observacoes`, `data_vigencia`, `meses_caucao`, `liquidado`) VALUES
+(8, '73943371000180', 'venda imovel', '22', '2024-11-25', '2024-12-04', 12, 'Cartão de crédito', NULL, NULL, NULL, NULL),
+(10, '73943371000180', 'venda imovel', '22', '2024-11-20', '2024-12-05', 12, 'Cartão de débito', NULL, NULL, NULL, NULL),
+(12, '73943371000180', 'venda imovel', '22', '2024-11-25', '2024-11-27', 12, 'Cartão de crédito', NULL, NULL, NULL, NULL),
+(13, '73943371000180', 'Aluguel', '22', '2024-11-06', '2024-10-30', 33, 'Dinheiro', '3', NULL, NULL, NULL),
+(14, '73943371000180', 'venda imovel', '22', '2024-11-25', '2024-11-29', 12, 'PIX', NULL, NULL, NULL, NULL),
+(15, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-22', NULL, 'Boleto', NULL, '2024-11-06', NULL, NULL),
+(16, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-08', 12, 'Boleto', NULL, '2024-12-06', NULL, NULL),
+(17, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-22', 500001, 'Cartão de débito', NULL, '2024-12-04', NULL, NULL),
+(18, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-12-06', 12, 'PIX', NULL, '2024-11-27', NULL, NULL),
+(19, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-20', 12, 'PIX', NULL, '2024-11-27', NULL, NULL),
+(20, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-20', 12, 'PIX', NULL, '2024-11-27', NULL, NULL),
+(21, '73943371000180', 'aluguel fiador', '22', '2024-11-26', '2024-11-20', 12, 'PIX', NULL, '2024-11-27', NULL, NULL),
+(34, '73943371000180', 'aluguel caucao', '22', '2024-11-26', '2024-11-28', 11, 'Cartão de crédito', NULL, '2024-11-29', NULL, NULL),
+(35, '73943371000180', 'aluguel caucao', '22', '2024-11-26', '2024-12-06', 2222222, 'Boleto', NULL, NULL, 11, NULL),
+(36, '73943371000180', 'aluguel caucao', '22', '2024-11-26', '2024-12-06', 11, 'Cartão de crédito', NULL, '2024-11-29', NULL, NULL),
+(37, '73943371000180', 'aluguel caucao', '22', '2024-11-26', '2024-11-07', 11, 'PIX', NULL, NULL, 1, NULL),
+(38, '73943371000180', 'Reparos', '22', '2024-11-29', '2024-11-26', 22, 'Cartão de crédito', '22', NULL, NULL, NULL),
+(39, '73943371000180', 'aluguel caucao', '22', '2024-11-26', '2024-11-29', 11, 'Cartão de crédito', NULL, '2024-12-06', NULL, NULL),
+(40, '73943371000180', 'aluguel caucao', '22', '2024-11-26', '2024-12-06', 110, 'PIX', NULL, NULL, 10, NULL),
+(41, '73943371000180', 'Reparos', '22', '2024-11-27', '2024-11-21', -33, 'Cartão de crédito', 'teste de negativo', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -424,7 +439,7 @@ ALTER TABLE `cadastro_imovel`
 -- AUTO_INCREMENT de tabela `contrato_aluguel`
 --
 ALTER TABLE `contrato_aluguel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `contrato_venda`
@@ -436,7 +451,7 @@ ALTER TABLE `contrato_venda`
 -- AUTO_INCREMENT de tabela `lancamento_financeiro`
 --
 ALTER TABLE `lancamento_financeiro`
-  MODIFY `id_lancamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_lancamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de tabela `superior`
